3a1176e922c4f9c517ae14edc9fe2aaa11b8c2ab
Prashanth Ravichandar <rprash99@gmail.com>
2024-03-19 13:34:39 -0700

    Add code to plot confidence intervals and remove bug while saving

--- code/main.py
+++ code/main.py
@@ -2,6 +2,8 @@ import rooms
 import random
 import agent as a
 import matplotlib.pyplot as plot
+import seaborn as sns
+import pandas as pd
 import sys
 from utils import save_agent, load_agent
 import numpy as np
@@ -11,6 +13,14 @@ def plot_returns(x,y):
     plot.title("Progress")
     plot.xlabel("Episode")
     plot.ylabel("Discounted Return")
+    plot.show()
+
+def plot_eval_returns(x, y):
+    df = pd.DataFrame(y)
+    df = df.melt(var_name="Episode", value_name="Discounted Return") # lineplot expects data in long format
+    sns.lineplot(x="Episode", y="Discounted Return", data=df, errorbar='ci', ci=95)
+    plot.axhline(y=0.8, color='black', linestyle='--')
+    plot.title("Evaluation returns")
     plot.show()
 
 def episode(env, agent, nr_episode=0, evaluation_mode=False, verbose=True):
@@ -37,7 +47,19 @@ def episode(env, agent, nr_episode=0, evaluation_mode=False, verbose=True):
     if verbose: print(nr_episode, ":", discounted_return, "steps: ", time_step)
     return discounted_return
 
+def train(env, agent, episodes):
+    returns = [episode(env, agent, nr_episode=i, verbose=True) for i in range(episodes)]
+    return returns
 
+def evaluate(env, agent, runs, episodes):
+    eval_returns = []
+    for i in range(no_runs):
+        returns = [episode(env, agent, nr_episode=i, verbose=False, evaluation_mode=True) for i in range(episodes)]
+        eval_returns.append(returns)
+    return np.array(eval_returns)
+
+np.random.seed(42)
+random.seed(42)
 params = {}
 rooms_instance = sys.argv[1]
 env = rooms.load_env(f"layouts/{rooms_instance}.txt", f"{rooms_instance}.mp4")
@@ -54,23 +76,21 @@ params["epsilon"] = 1
 # agent = a.QLearner(params)
 agent = a.UCBQLearner(params)
 
-# agent = load_agent("saved_agents/agent: 2024-03-11 19:10:22.pkl") # Load agent from file
 training_episodes = 200
 evaluation_episodes = 10
+no_runs = 100
 
 # TRAINING
-returns = [episode(env, agent, nr_episode=i, verbose=True) for i in range(training_episodes)]
-x = range(training_episodes)
-y = returns
-plot_returns(x,y)
+returns = train(env, agent, training_episodes)
+plot_returns(x=range(training_episodes),y=returns)
+# save_agent(agent)
+# exit()
 
 # EVALUATION
-eval_returns = [episode(env, agent, nr_episode=i, verbose=True, evaluation_mode=True) for i in range(evaluation_episodes)]
-x_eval = range(evaluation_episodes)
-y_eval = eval_returns
-plot_returns(x_eval,y_eval)
+# agent = load_agent("saved_agents/agent: 2024-03-19 13:01:51.pkl")
+eval_returns = evaluate(env, agent, runs=no_runs, episodes=evaluation_episodes)
+plot_eval_returns(x=range(evaluation_episodes), y=eval_returns)
 
 print(f"Average evaluation discounted return: {np.mean(eval_returns)}")
-# save_agent(agent)
 
 env.save_video()
--- code/utils.py
+++ code/utils.py
@@ -1,8 +1,9 @@
 import pickle
 from datetime import datetime
-
+import os
 
 def save_agent(agent, filename="agent"):
+    os.makedirs("saved_agents", exist_ok=True)
     current_datetime = datetime.now()
     date_string = current_datetime.strftime("%Y-%m-%d %H:%M:%S")
     with open(f"saved_agents/{filename}: {date_string}.pkl", 'wb') as file:
@@ -11,5 +12,6 @@ def save_agent(agent, filename="agent"):
 def load_agent(path, verbose=False):
     with open(path, 'rb') as file:
         agent = pickle.load(file)
-    if verbose: agent.print()
+    if verbose: 
+        agent.print()
     return agent
